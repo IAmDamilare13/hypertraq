8bbd4beda1c52cf389882469f4d7d00bd4235494
Damilare DARAMOLA <86056544+IAmDamilare13@users.noreply.github.com>
2024-08-29 22:52:14 +0100

    Update commons.py

--- models/commons.py
+++ models/commons.py
@@ -3,11 +3,6 @@ import time, logging, string, random
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger("    ")
 
-MONGODB = "mongodb+srv://daramolacpe186651:T8vhI4cfu8zyDddO@cluster0.maqru.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# MONGODB = "mongodb://localhost:27017"
-
-
 def get_timestamp():
     return int(time.time())
 
